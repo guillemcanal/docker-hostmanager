94a0b289c98c18d4ddfa5814ccae278c681dc423
Guillem CANAL <gcanal@eleven-labs.com>
2018-06-04 13:48:45 +0200

    fix: Simplify Docker image creation

--- scripts/docker-push.sh
+++ scripts/docker-push.sh
@@ -3,13 +3,31 @@
 set -e
 
 main() {
-    local DOCKER_TAG="${1:-latest}"
+    local PROJECT_NAME="docker-hostmanager"
     [ -n "$DOCKER_USERNAME" ] || { echo 'DOCKER_USERNAME need to be declared'; exit 1; }
     [ -n "$DOCKER_PASSWORD" ] || { echo 'DOCKER_PASSWORD need to be declared'; exit 1; }
 
     echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker build -t gcanal/docker-hostmanager:$DOCKER_TAG .
-    docker push gcanal/docker-hostmanager:$DOCKER_TAG
+
+    docker build -t $DOCKER_USERNAME/$PROJECT_NAME:latest .
+    docker push $DOCKER_USERNAME/$PROJECT_NAME:latest
+
+    # Tag image
+    if [ -n "$TRAVIS_TAG" ];then
+        # Extract major, minor and revision from tag
+        local PARTS=( ${TRAVIS_TAG//./ } )
+        local MAJOR=${PARTS[0]}
+        local MINOR=${PARTS[1]}
+        local REVISION=${PARTS[2]}
+        
+        docker tag $DOCKER_USERNAME/$PROJECT_NAME:latest $DOCKER_USERNAME/$PROJECT_NAME:$MAJOR
+        docker tag $DOCKER_USERNAME/$PROJECT_NAME:latest $DOCKER_USERNAME/$PROJECT_NAME:$MAJOR.$MINOR
+        docker tag $DOCKER_USERNAME/$PROJECT_NAME:latest $DOCKER_USERNAME/$PROJECT_NAME:$MAJOR.$MINOR.$REVISION
+
+        docker push $DOCKER_USERNAME/$PROJECT_NAME:$MAJOR
+        docker push $DOCKER_USERNAME/$PROJECT_NAME:$MAJOR.$MINOR
+        docker push $DOCKER_USERNAME/$PROJECT_NAME:$MAJOR.$MINOR.$REVISION
+    fi
 }
 
 main "$@"
